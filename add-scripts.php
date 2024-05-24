d28271709d437ef13707cb138ef955e6345b7d43
ホセ <38702514+josejtax@users.noreply.github.com>
2024-05-24 12:27:26 +0200

    fix WordPress.Security.EscapeOutput

--- add-scripts.php
+++ add-scripts.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name:       Scriptify | Scripts in head/footer/body
- * Plugin URI:        https://github.com/jt4x/scriptify
+ * Plugin URI:        https://github.com/josejtax/scriptify
  * Description:       Add global code to your website with Scriptify. Analytics code, new styles, user alerts, and much more.
  * Version:           1.0
  * Requires at least: 6.3
@@ -10,8 +10,7 @@
  * Author URI:        https://jtax.dev
  * License:           GPL v3 or later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
- * Update URI:        https://github.com/jt4x/scriptify
- * Text Domain:       scriptify
+ * Text Domain:       scriptify-main
  */
 
 defined('ABSPATH') or die('You shouldnt be here...');
@@ -25,25 +24,26 @@ add_action('admin_menu', 'vantages_add_backend');
 function front_scriptify() {
     echo "<h1>Scriptify</h1>";
     echo "<hr style='border: 1px solid #000'>";
-    if ($_POST) {
-        update_option('script_head', $_POST["script_head_options"], '', 'yes');
-        update_option('script_body', $_POST["script_body_options"], '', 'yes');
-        update_option('script_footer', $_POST["script_footer_options"], '', 'yes');
+    if ($_POST && check_admin_referer('scriptify_nonce_action', 'scriptify_nonce_field')) {
+        update_option('script_head', sanitize_text_field($_POST["script_head_options"]), '', 'yes');
+        update_option('script_body', sanitize_text_field($_POST["script_body_options"]), '', 'yes');
+        update_option('script_footer', sanitize_text_field($_POST["script_footer_options"]), '', 'yes');
     }
     ?>
     <form action="#" method="post">
+        <?php wp_nonce_field('scriptify_nonce_action', 'scriptify_nonce_field'); ?>
         <h3>Head</h3>
-        <textarea style='width: 50%; height: 300px;' name="script_head_options"><?= stripslashes(get_option('script_head', '')) ?></textarea>
+        <textarea style='width: 50%; height: 300px;' name="script_head_options"><?php echo esc_textarea(get_option('script_head', '')); ?></textarea>
         <hr>
         <h3>Body</h3>
-        <textarea style='width: 50%; height: 300px;' name="script_body_options"><?= stripslashes(get_option('script_body', '')) ?></textarea>
+        <textarea style='width: 50%; height: 300px;' name="script_body_options"><?php echo esc_textarea(get_option('script_body', '')); ?></textarea>
         <hr><br>
         <h3>Footer</h3>
-        <textarea style='width: 50%; height: 300px;' name="script_footer_options"><?= stripslashes(get_option('script_footer', '')) ?></textarea>
+        <textarea style='width: 50%; height: 300px;' name="script_footer_options"><?php echo esc_textarea(get_option('script_footer', '')); ?></textarea>
         <br>
         <input type="submit" value="SAVE">
     </form>
-    <?php
+    <?php    
 }
 
 add_action('wp_head', 'add_scriptify_head');
@@ -77,3 +77,8 @@ function scriptify_enqueue_block_editor_assets() {
     );
 }
 add_action('enqueue_block_editor_assets', 'scriptify_enqueue_block_editor_assets');
+
+function scriptify_enqueue_scripts() {
+    wp_enqueue_script('scriptify-custom-script', plugins_url('/js/custom-script.js', __FILE__), array(), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'scriptify_enqueue_scripts');
